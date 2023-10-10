848cc5afc4ff2ca610be46ade306c22f87a8fbc6
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-10 21:18:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
 class Formatter {
-  static capitalize(str) {
-    if (typeof str !== 'string' || str.length === 0) {
-      throw new Error('Input must be a non-empty string');
-    }
-
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  static sanitize(str) {
-    if (typeof str !== 'string') {
-      throw new Error('Input must be a string');
-    }
-
-    // Remove non-alphanumeric characters except dash, single quote, and space
-    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
   }
 
-  static titleize(str) {
-    if (typeof str !== 'string') {
-      throw new Error('Input must be a string');
-    }
+  static titleize(sentence) {
+    const stopWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = sentence.split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !stopWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
 
-    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
-
-    return str
-      .split(' ')
-      .map((word, index) => {
-        if (index === 0 || !exceptions.includes(word.toLowerCase())) {
-          return Formatter.capitalize(word);
-        } else {
-          return word.toLowerCase();
-        }
-      })
-      .join(' ');
+    return titleizedWords.join(' ');
   }
 }
 
